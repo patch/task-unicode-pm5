2b761171e03fda9c6482b38b0b50a96739a15ee0
Nick Patch <patch@cpan.org>
2012-01-15 18:37:48 -0500

    new project

---
+++ task-unicode-pm5/lib/Task/Unicode.pm
@@ -0,0 +1,38 @@
+package Task::Unicode;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Unicode - Everything needed to work with Unicode
+
+=head1 VERSION
+
+This document describes Task::Unicode version 0.01.
+
+=head1 DESCRIPTION
+
+...
+
+=head1 AUTHOR
+
+Nick Patch <patch@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+© 2012 Nick Patch
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
