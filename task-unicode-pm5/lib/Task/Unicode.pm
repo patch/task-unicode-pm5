d633d67deda23ae4e6c4bc3d10466e8e4970dc69
Nick Patch <patch@cpan.org>
2012-04-11 22:35:30 -0400

    release 0.04

--- task-unicode-pm5/lib/Task/Unicode.pm
+++ task-unicode-pm5/lib/Task/Unicode.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 1;
 
@@ -18,7 +18,7 @@ Task::Unicode - Everything needed to work with Unicode data
 
 =head1 VERSION
 
-This document describes Task::Unicode version 0.03.
+This document describes Task::Unicode version 0.04.
 
 =head1 DESCRIPTION
 
